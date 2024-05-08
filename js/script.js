e31c572cd68a989d1d61b2ed02438d33b8c76286
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-08 13:29:03 +0200

    added structure at the script

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@
 //     va applicato uno sconto del 21% per i minorenni
 //     va applicato uno sconto del 42% per gli over 65.
 //     L'output del prezzo finale va messo fuori in forma umana (con massimo due   decimali, per indicare centesimi sul prezzo) [questo richiederà un minimo di ricerca]
+
+// chiedi il numero di chilometri
+    // deve essere un numero maggiore di 0
+
+// chiedi l'eta del passeggero
+    // deve essere un numero maggiore di 0
+
+const prezzoPerKm = 0.276 // (in euro)
+
+// se eta < di 18: sconto del 20%
+
+// se eta >= 65: sconto del 42%
+
+//  output
