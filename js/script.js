022f04618855a07cfd46a87f88bd0b4fe6bccdb0
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-08 13:48:29 +0200

    added script

--- js/script.js
+++ js/script.js
@@ -7,15 +7,39 @@
 //     L'output del prezzo finale va messo fuori in forma umana (con massimo due   decimali, per indicare centesimi sul prezzo) [questo richiederà un minimo di ricerca]
 
 // chiedi il numero di chilometri
+let km = Number.parseInt(prompt("Ciao, scrivi qui il numero di chilometri che devi percorrere")); 
     // deve essere un numero maggiore di 0
+if (isNaN(km) || km <= 0) {
+    km = Math.floor(Math.random() * 1000 + 1);
+    document.getElementById("km").innerHTML = ("Non sono riuscito a capire cio' che mi hai detto, quindi ti faro' un esempio per " + km + " km.")
+} else {
+    document.getElementById("km").innerHTML = ("I chilometri che deve percorrere il passeggero sono " + km + ".")
+}
 
 // chiedi l'eta del passeggero
+let eta =  Number.parseInt(prompt("Perfetto, ora scrivi la tua eta"));
     // deve essere un numero maggiore di 0
+if (isNaN(eta) || eta <= 0) {
+    eta = Math.floor(Math.random() * 100 + 1);
+    document.getElementById("eta").innerHTML = ("Non sono riuscito a capire cio' che mi hai detto, quindi ti faro' un esempio per una persona di " + eta + " anni.")
+} else {
+    document.getElementById("eta").innerHTML = ("Il passeggero ha " + eta + " anni.")
+}
 
 const prezzoPerKm = 0.276 // (in euro)
+let prezzoBiglietto = prezzoPerKm * km;
 
 // se eta < di 18: sconto del 20%
+if (eta < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.79;
+}
 
 // se eta >= 65: sconto del 42%
+if (eta >= 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.58;
+}
+
+prezzoBiglietto = Math.round(prezzoBiglietto * 100) / 100
 
 //  output
+document.getElementById("prezzo").innerHTML = ("Il prezzo del biglietto per un passeggero con queste caratteristiche e' di: " + prezzoBiglietto + "€")
